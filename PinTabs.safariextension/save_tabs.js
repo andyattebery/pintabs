8b4a43fa2da1557a53d5d0224bda1e6c5205b579
Kevin Kinnebrew <kevin.kinnebrew@gmail.com>
2014-01-22 13:33:25 -0600

    Unhacking

--- PinTabs.safariextension/save_tabs.js
+++ PinTabs.safariextension/save_tabs.js
@@ -1,33 +1,48 @@
-safari.application.addEventListener("popover", handlePopoverEvent, false);
+(function() {
 
-function handlePopoverEvent(event)
-{  
-  function addTabToList(list, title) {
-    var tabListItem = document.createElement('li');
-    tabListItem.appendChild(document.createTextNode(title));
-    list.appendChild(tabListItem);
-  }
-  
-  function submitTags() {
-    var tabs = safari.application.activeBrowserWindow.tabs;
-    tabs.forEach(function(element, index, array) {
-      console.log(element.url);
+  function onLoad() {
+    
+    console.log("loaded");
+    
+    var form = document.getElementById("tagsForm");
+    
+    function submitTags() {
+      var tabs = safari.application.activeBrowserWindow.tabs;
+      tabs.forEach(function(element, index, array) {
+        console.log(element.url);
+      });
+    }
+    
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      submitTags();
     });
+    
   }
   
-  var tabs = safari.application.activeBrowserWindow.tabs;
-  var tabsList = document.getElementById("tabsList");
-  var tabsListItems = tabsList.getElementsByTagName("li");
-  tabsListItems.forEach(function(element, index, array) {
-    tabsList.removeChild(element);
-  });
-  tabs.forEach(function(element, index, array) {
-    addTabToList(tabsList, element.title)
-  });
+  function onPopover(event) {
+    
+    function addTabToList(list, title) {
+      var tabListItem = document.createElement('li');
+      tabListItem.appendChild(document.createTextNode(title));
+      list.appendChild(tabListItem);
+    }
+    
+    var tabs = safari.application.activeBrowserWindow.tabs;
+    var tabsList = document.getElementById("tabsList");
+    var tabsListItems = tabsList.getElementsByTagName("li");
+    
+    for (var i = 0, len = tabListItems.length; i < len; i++) {
+      tabsList.removeChild(tabListItems[i]);
+    }
+    
+    tabs.forEach(function(element, index, array) {
+      addTabToList(tabsList, element.title)
+    });
+    
+  }
   
-  var form = document.getElementById("tagsForm");
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    submitTags();
-  });
-}
+  safari.application.addEventListener("popover", onPopover, false);
+  window.onload = onLoad;
+
+}());
